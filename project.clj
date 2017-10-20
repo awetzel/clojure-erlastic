63759101350f05d82e7627bee9c83c3808402aef
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2017-10-20 21:47:30 +0100

    Remove reflection warnings.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :scm {:name "git" :url "https://github.com/awetzel/clojure-erlastic"}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.erlang.otp/jinterface "1.5.9"]]
--- src/clojure_erlastic/core.clj
+++ src/clojure_erlastic/core.clj
@@ -1,80 +1,102 @@
-(ns clojure-erlastic.core)
-(import '(com.ericsson.otp.erlang OtpErlangAtom OtpErlangObject OtpErlangTuple
-                                  OtpErlangString OtpErlangList OtpErlangLong
-                                  OtpErlangBinary OtpErlangDouble OtpErlangMap
-                                  OtpErlangDecodeException OtpInputStream OtpOutputStream OtpException)
-        '(java.nio.charset Charset) '(java.nio ByteBuffer CharBuffer))
-(require '[clojure.core.async :as async :refer [<! >! <!! chan go close!]])
+(ns clojure-erlastic.core
+  (:require [clojure.core.async :as async :refer [<! >! <!! chan go close!]])
+  (:import (com.ericsson.otp.erlang OtpErlangAtom OtpErlangObject OtpErlangTuple
+                                    OtpErlangString OtpErlangList OtpErlangLong
+                                    OtpErlangBinary OtpErlangDouble OtpErlangMap
+                                    OtpErlangDecodeException OtpInputStream OtpOutputStream
+                                    OtpException)
+           (java.nio.charset Charset)
+           (java.nio ByteBuffer CharBuffer)
+           (sun.nio.cs UTF_8)))
 
 ;; CONFIGURATION
-(def default-config {
-  :str-detect :none
-  :convention :elixir
-  :str-autodetect-len 10 })
-(defn get-conf [config key] 
+(def default-config {:str-detect :none
+                     :convention :elixir
+                     :str-autodetect-len 10})
+
+(defn get-conf [config key]
   (if (contains? config key) (config key) (default-config key)))
 
 ;; String conversion and detection utility functions
-(def utf-8 "The UTF-8 charset object" (Charset/forName "UTF-8"))
-(defn printable? [c]
-  (#{Character/UPPERCASE_LETTER Character/TITLECASE_LETTER Character/SPACE_SEPARATOR Character/PARAGRAPH_SEPARATOR Character/LOWERCASE_LETTER 
-     Character/CURRENCY_SYMBOL Character/DECIMAL_DIGIT_NUMBER
-     Character/DASH_PUNCTUATION Character/CONNECTOR_PUNCTUATION Character/END_PUNCTUATION Character/FINAL_QUOTE_PUNCTUATION Character/INITIAL_QUOTE_PUNCTUATION Character/START_PUNCTUATION Character/OTHER_PUNCTUATION} 
-      (Character/getType c)))
+(def ^UTF_8 utf-8 "The UTF-8 charset object" (Charset/forName "UTF-8"))
 
-(defn bin-str? [bin detect-len]
-  (let [char-res (CharBuffer/allocate detect-len)
-        len (min (alength bin) detect-len)
+(def ^:private printables #{Character/UPPERCASE_LETTER Character/TITLECASE_LETTER
+                            Character/SPACE_SEPARATOR
+                            Character/PARAGRAPH_SEPARATOR Character/LOWERCASE_LETTER
+                            Character/CURRENCY_SYMBOL Character/DECIMAL_DIGIT_NUMBER
+                            Character/DASH_PUNCTUATION Character/CONNECTOR_PUNCTUATION
+                            Character/END_PUNCTUATION Character/FINAL_QUOTE_PUNCTUATION
+                            Character/INITIAL_QUOTE_PUNCTUATION Character/START_PUNCTUATION
+                            Character/OTHER_PUNCTUATION})
+
+(defn printable? [c]
+  (printables (Character/getType c)))
+
+(defn bin-str? [^bytes bin detect-len]
+  (let [char-res  (CharBuffer/allocate detect-len)
+        len       (min (alength bin) detect-len)
         coder-res (.decode (.newDecoder utf-8) (ByteBuffer/wrap bin 0 len) char-res (= len (alength bin)))
-        char-len (.position char-res)]
-    (doto char-res (.rewind) (.limit char-len))
-    (and (not (.isError coder-res)) (every? printable? (.toString char-res)))))
+        char-len  (.position char-res)]
+    (doto char-res
+      (.rewind)
+      (.limit char-len))
+    (and (not (.isError coder-res))
+         (every? printable? (.toString char-res)))))
 
 (defn seq-str? [lst detect-len]
-  (every? #(and (Character/isValidCodePoint %) (printable? %)) (take detect-len lst)))
+  (every? #(and (Character/isValidCodePoint %)
+                (printable? %))
+          (take detect-len lst)))
 
-(defn str-or-bin [bin]
-  (try (String. bin utf-8)
-       (catch Exception e bin)))
+(defn str-or-bin [^bytes bin]
+  (try
+    (String. bin utf-8)
+    (catch Exception e bin)))
 
 (defn str-or-lst [lst]
-  (try (apply str (map char lst)) 
+  (try (apply str (map char lst))
        (catch Exception e lst)))
-        
+
 ;; Codec functions
-(defn decode 
+(defn decode
   ([obj] (decode obj default-config))
   ([obj config]
-    (let [t (type obj)
-          conf (fn [key] (get-conf config key))
-          dec (fn [obj] (decode obj config))]
-      (cond 
-        (= t OtpErlangAtom) (let [k (keyword (.atomValue obj))]
-          (cond
-            (#{:true :false} k) (= k :true) 
-            (and (= (conf :convention) :elixir) (= k :nil)) nil
-            (and (= (conf :convention) :erlang) (= k :undefined)) nil
-            :else k))
-        (= t OtpErlangList) (let [elem-seq (seq (map dec (.elements obj)))]
-          (cond 
-            (and (= (conf :convention) :erlang) (= (conf :str-detect) :all))
-              (str-or-lst elem-seq)
-            (and (= (conf :convention) :erlang) (= (conf :str-detect) :auto))
-              (if (seq-str? elem-seq (conf :str-autodetect-len)) (str-or-lst elem-seq) elem-seq)
-            :else elem-seq))
-        (= t OtpErlangTuple) (vec (map dec (.elements obj)))
-        (= t OtpErlangString) (.stringValue obj)
-        (= t OtpErlangBinary) (let [bin (.binaryValue obj)]
-          (cond 
-            (and (= (conf :convention) :elixir) (= (conf :str-detect) :all)) 
-              (str-or-bin bin)
-            (and (= (conf :convention) :elixir) (= (conf :str-detect) :auto))
-              (if (bin-str? bin (conf :str-autodetect-len)) (str-or-bin bin) bin)
-            :else bin))
-        (= t OtpErlangMap) (zipmap (map dec (.keys obj)) (map dec (.values obj)))
-        (= t OtpErlangLong) (.longValue obj)
-        (= t OtpErlangDouble) (.doubleValue obj)
-        :else   :decoding_error))))
+   (let [t (type obj)
+         conf (fn [key] (get-conf config key))
+         dec (fn [obj] (decode obj config))]
+     (cond
+       (= t OtpErlangAtom) (let [k (keyword (.atomValue ^OtpErlangAtom obj))]
+                             (cond
+                               (#{:true :false} k) (= k :true)
+                               (and (= (conf :convention) :elixir) (= k :nil)) nil
+                               (and (= (conf :convention) :erlang) (= k :undefined)) nil
+                               :else k))
+       (= t OtpErlangList) (let [elem-seq (seq (map dec (.elements ^OtpErlangList obj)))]
+                             (cond
+                               (and (= (conf :convention) :erlang) (= (conf :str-detect) :all))
+                               (str-or-lst elem-seq)
+                               (and (= (conf :convention) :erlang) (= (conf :str-detect) :auto))
+                               (if (seq-str? elem-seq (conf :str-autodetect-len)) (str-or-lst elem-seq) elem-seq)
+                               :else elem-seq))
+       (= t OtpErlangTuple) (vec (map dec (.elements ^OtpErlangTuple obj)))
+       (= t OtpErlangString) (.stringValue ^OtpErlangString obj)
+       (= t OtpErlangBinary) (let [bin (.binaryValue ^OtpErlangBinary obj)]
+                               (cond
+                                 (and (= (conf :convention) :elixir) (= (conf :str-detect) :all))
+                                 (str-or-bin bin)
+                                 (and (= (conf :convention) :elixir) (= (conf :str-detect) :auto))
+                                 (if (bin-str? bin (conf :str-autodetect-len)) (str-or-bin bin) bin)
+                                 :else bin))
+       (= t OtpErlangMap) (zipmap (map dec (.keys ^OtpErlangMap obj))
+                                  (map dec (.values ^OtpErlangMap obj)))
+       (= t OtpErlangLong) (.longValue ^OtpErlangLong obj)
+       (= t OtpErlangDouble) (.doubleValue ^OtpErlangDouble obj)
+       :else   :decoding_error))))
+
+(defn- erlang-objects
+  #^"[Lcom.ericsson.otp.erlang.OtpErlangObject;"
+  [xs]
+  (into-array OtpErlangObject xs))
 
 (defn encode
   ([obj] (encode obj default-config))
@@ -83,21 +105,21 @@
            (enc [obj] (encode obj config))]
      (cond
        (nil? obj)
-         (if (= (conf :convention) :elixir)
-             (OtpErlangAtom. "nil")
-             (OtpErlangAtom. "undefined"))
+       (if (= (conf :convention) :elixir)
+         (OtpErlangAtom. "nil")
+         (OtpErlangAtom. "undefined"))
        (char? obj) (enc (int obj))
-       (seq? obj) (OtpErlangList. (into-array OtpErlangObject (map enc obj)))
-       (set? obj) (OtpErlangList. (into-array OtpErlangObject (map enc obj)))
-       (vector? obj) (OtpErlangTuple. (into-array OtpErlangObject (map enc obj)))
-       (string? obj) 
-         (if (= (conf :convention) :elixir) 
-             (OtpErlangBinary. (bytes (.getBytes obj "UTF-8")))
-             (enc (seq obj)))
+       (seq? obj) (OtpErlangList. (erlang-objects (map enc obj)))
+       (set? obj) (OtpErlangList. (erlang-objects (map enc obj)))
+       (vector? obj) (OtpErlangTuple. (erlang-objects (map enc obj)))
+       (string? obj)
+       (if (= (conf :convention) :elixir)
+         (OtpErlangBinary. (bytes (.getBytes ^String obj "UTF-8")))
+         (enc (seq obj)))
        (keyword? obj) (OtpErlangAtom. (name obj))
        (integer? obj) (OtpErlangLong. (long obj))
        (float? obj) (OtpErlangDouble. (double obj))
-       (map? obj) (OtpErlangMap. (into-array OtpErlangObject (map enc (keys obj))) 
+       (map? obj) (OtpErlangMap. (into-array OtpErlangObject (map enc (keys obj)))
                                  (into-array OtpErlangObject (map enc (vals obj))))
        (= (Class/forName "[B") (type obj)) (OtpErlangBinary. (bytes obj))
        (= java.lang.Boolean (type obj)) (enc (keyword (str obj)))
@@ -107,47 +129,57 @@
 (defn log [& params]
   (.println System/err (apply str params)))
 
-(defn port-connection 
+(defn port-connection
   ([] (port-connection default-config))
   ([config]
-    (let [in (chan) out (chan)]
-      (go ;; term receiver coroutine
-        (try 
-          (while true 
-            (let [len-buf (byte-array 4)]
-              (.read System/in len-buf)
-              (let [term-len (.read4BE (new OtpInputStream len-buf))
-                    term-buf (byte-array term-len)]
-                (.read System/in term-buf)
-                (let [b (decode (.read_any (new OtpInputStream term-buf)) config) ]
-                  (>! in b)))))
-          (catch Exception e (do 
-            (log "receive error : " (type e) " " (.getMessage e)) 
-            (close! out) (close! in)))))
-      (go ;; term sender coroutine
-        (loop []
-          (when-let [term (<! out)]
-            (try
-              (let [out-term (new OtpOutputStream (encode term config))]
-                (doto (new OtpOutputStream) (.write4BE (+ 1 (.size out-term))) (.write 131) (.writeTo System/out))
-                (.writeTo out-term System/out)
-                (.flush System/out))
-              (catch Exception e (log "send error : " (type e) " " (.getMessage e))))
-            (recur))))
-      [in out] )))
+   (let [in (chan) out (chan)]
+     (go ;; term receiver coroutine
+       (try
+         (while true
+           (let [len-buf (byte-array 4)]
+             (.read System/in len-buf)
+             (let [term-len (.read4BE (OtpInputStream. len-buf))
+                   term-buf (byte-array term-len)]
+               (.read System/in term-buf)
+               (let [b (decode (.read_any (OtpInputStream. term-buf)) config)]
+                 (>! in b)))))
+         (catch Exception e
+           (do
+             (log "receive error : " (type e) " " (.getMessage ^Exception e))
+             (close! out)
+             (close! in)))))
+     (go ;; term sender coroutine
+       (loop []
+         (when-let [term (<! out)]
+           (try
+             (let [out-term (OtpOutputStream. ^OtpErlangObject (encode term config))]
+               (doto (OtpOutputStream.)
+                 (.write4BE (+ 1 (.size out-term)))
+                 (.write 131)
+                 (.writeTo System/out))
+               (.writeTo out-term System/out)
+               (.flush System/out))
+             (catch Exception e (log "send error : " (type e) " " (.getMessage e))))
+           (recur))))
+     [in out])))
 
 ;; erlang style genserver management
-(defn run-server 
+(defn run-server
   ([handle] (run-server (fn [state] state) handle default-config))
   ([init handle] (run-server init handle default-config))
   ([init handle config]
-    (let [[in out] (port-connection config)]
-      (<!! (go
-        (loop [state (init (<! in))]
-          (if-let [req (<! in)]
-            (let [res (try (handle req state) (catch Exception e [:stop [:error e]]))]
-              (case (res 0)
-                :reply (do (>! out (res 1)) (recur (res 2)))
-                :noreply (recur (res 1))
-                :stop (res 1)))
-            :normal)))))))
+   (let [[in out] (port-connection config)]
+     (<!! (go
+            (loop [state (init (<! in))]
+              (if-let [req (<! in)]
+                (let [res (try
+                            (handle req state)
+                            (catch Exception e
+                              [:stop [:error e]]))]
+                  (case (res 0)
+                    :reply (do
+                             (>! out (res 1))
+                             (recur (res 2)))
+                    :noreply (recur (res 1))
+                    :stop (res 1)))
+                :normal)))))))
